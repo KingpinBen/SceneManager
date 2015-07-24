a353f66365bf7e575e102e7f183a993e9848866d
Ben Griffiths <scare_tfc@hotmail.com>
2015-07-24 03:27:04 +0100

    Added script to be placed at root object in scenes
    The script registers the base object of the screen so that things can be
    properly disabled/removed once added.

---
+++ Assets/SceneManager/ManagedSceneObject.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using System.IO;
+#endif
+
+
+public class ManagedSceneObject : MonoBehaviour
+{
+    [SerializeField]
+    private string _sceneName;
+
+    private void Start()
+    {
+        SceneManager.instance.RegisterSceneObject(this);
+    }
+
+    public string sceneName
+    {
+        get { return _sceneName; }
+    }
+
+#if UNITY_EDITOR
+    private void Reset()
+    {
+        _sceneName = 
+            Path.GetFileNameWithoutExtension(EditorApplication.currentScene);
+    }
+#endif
+}
