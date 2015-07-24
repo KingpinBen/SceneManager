c0545e398dfcc54514d8b3a1b9526507967cdbe6
Ben Griffiths <scare_tfc@hotmail.com>
2015-07-24 03:26:14 +0100

    Updated the example scenes
    The player can now move around and watch scenes load in and out by
    pressing the A/D keys to move a ball

---
+++ Assets/scripts/BallPush.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallPush : MonoBehaviour
+{
+    private Rigidbody2D _rigidBody;
+
+    private void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+	void Start () {
+	
+	}
+
+	void Update ()
+    {
+        const float force = 10;
+        var h = Input.GetAxis("Horizontal");
+        _rigidBody.AddForce(new Vector2(force * h, 0), ForceMode2D.Force);
+	}
+}
---
+++ Assets/scripts/CameraFollowTarget.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowTarget : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _target;
+
+	void LateUpdate ()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, idealPosition, 1.0f);
+	}
+
+    private Vector3 idealPosition
+    {
+        get { return new Vector3(_target.position.x, _target.position.y, -10); }
+    }
+}
