5a5ad9aadc1dc1377aba7ada0dd1f10e06b31b2f
Ben Griffiths <scare_tfc@hotmail.com>
2015-07-24 21:57:33 +0100

    Added support for traversing objects

--- Assets/SceneManager/SceneManager.cs
+++ Assets/SceneManager/SceneManager.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿//#define DEBUG_SCREENMANAGER
+
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -6,7 +8,7 @@ public sealed class SceneManager : MonoBehaviour
 {
     [SerializeField]
     private SceneManagerAsset _sceneManagerAsset;
-    private Dictionary<int, GameObject> _sceneBaseObjects;
+    private Dictionary<int, ManagedSceneObject> _sceneBaseObjects;
     private HashSet<int> _activeScenes;
     private float _sqrBufferArea;
     private float _sqrActiveArea;
@@ -24,7 +26,7 @@ public sealed class SceneManager : MonoBehaviour
         {
             DontDestroyOnLoad(this);
 
-            _sceneBaseObjects = new Dictionary<int, GameObject>();
+            _sceneBaseObjects = new Dictionary<int, ManagedSceneObject>();
             _activeScenes = new HashSet<int>();
 
             _sqrBufferArea = Mathf.Pow(cBufferArea, 2);
@@ -59,25 +61,27 @@ public sealed class SceneManager : MonoBehaviour
 
     private void UnloadScene(int sceneIndexInAsset)
     {
-        GameObject go;
+        ManagedSceneObject obj;
 
-        if (_sceneBaseObjects.TryGetValue(sceneIndexInAsset, out go))
+        if (_sceneBaseObjects.TryGetValue(sceneIndexInAsset, out obj))
         {
-            Destroy(go);
+            Destroy(obj.gameObject);
             _sceneBaseObjects.Remove(sceneIndexInAsset);
             _activeScenes.Remove(sceneIndexInAsset);
         }
     }
 
-    public void RegisterSceneObject(ManagedSceneObject obj)
+    public SceneData RegisterSceneObject(ManagedSceneObject obj)
     {
         if (obj == null)
-            return;
+            throw new UnityException("SCENEMANAGER::REGISTERSCENEOBJECT::MANAGEDSCENEOBJECT_IS_NULL");
+
         int index;
         if (!_sceneManagerAsset.TryFindSceneIndex(obj.sceneName, out index))
             throw new UnityException("SCENEMANAGER::REGISTERSCENEOBJECT::SCENEMANAGERASSET_DOESNT_CONTAIN_VALUE");
 
-        _sceneBaseObjects[index] = obj.gameObject;
+        _sceneBaseObjects[index] = obj;
+        return _sceneManagerAsset[index];
     }
 
     private IEnumerator CalculateScenesStatesPeriodically()
@@ -107,21 +111,27 @@ public sealed class SceneManager : MonoBehaviour
             if (!_sceneBaseObjects.ContainsKey(index))
                 return;
 
-            if (_sceneBaseObjects[index].activeSelf)
+            if (_sceneBaseObjects[index].gameObject.activeSelf)
             {
                 if ((closestPoint - trackedObjectPos).sqrMagnitude > _sqrActiveArea)
-                    _sceneBaseObjects[index].SetActive(false);
+                {
+                    _sceneBaseObjects[index].TryTransferTraversedObjects();
+                    _sceneBaseObjects[index].gameObject.SetActive(false);
+                }
             }
             else
             {
                 if ((closestPoint - trackedObjectPos).sqrMagnitude < _sqrActiveArea)
                 {
-                    _sceneBaseObjects[index].SetActive(true);
+                    _sceneBaseObjects[index].gameObject.SetActive(true);
                 }
                 else
                 {
                     if ((closestPoint - trackedObjectPos).sqrMagnitude > _sqrBufferArea)
+                    {
+                        _sceneBaseObjects[index].TryTransferTraversedObjects();
                         UnloadScene(index);
+                    }
                 }
             }
         }
@@ -142,13 +152,25 @@ public sealed class SceneManager : MonoBehaviour
         return ClosestPointToRect(ref rect, ref from);
     }
 
-    private void OnDrawGizmos()
+    public bool TryTransferTraversable(Renderer traversableRenderer)
     {
-        for(int i = 0; i < _sceneManagerAsset.Count; i++)
+        for(int i = 0; i <_activeScenes.Count; i++)
         {
-            Gizmos.DrawLine(targetPositionAsVec2, ClosestPointToRect(ref _sceneManagerAsset[i].rectangle, targetPositionAsVec2));
-            Gizmos.DrawWireCube(_sceneManagerAsset[i].alteredCenter, new Vector3(_sceneManagerAsset[i].rectangle.width, _sceneManagerAsset[i].rectangle.height));
+            Debug.Log(string.Format("{0}, {1} | {2}, {3}",
+                _sceneManagerAsset[i].rectangle.x,
+                _sceneManagerAsset[i].rectangle.y,
+                _sceneManagerAsset[i].rectangle.xMax,
+                _sceneManagerAsset[i].rectangle.yMax));
+
+            if (_sceneManagerAsset[i].rectangle.Contains(traversableRenderer.bounds))
+            {
+                _sceneBaseObjects[i].ReceiveTraversableObject(traversableRenderer);
+                Debug.Log(string.Format("Transferring '{0}' to scene: '{1}'", traversableRenderer.name, _sceneManagerAsset[i].name));
+                return true;
+            }
         }
+
+        return false;
     }
 
     public int ActiveSceneCount
@@ -180,6 +202,17 @@ public sealed class SceneManager : MonoBehaviour
     {
         get { return _sceneManagerAsset; }
         set { _sceneManagerAsset = value; }
+    }
+#endif
+
+#if DEBUG_SCREENMANAGER && UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        for (int i = 0; i < _sceneManagerAsset.Count; i++)
+        {
+            Gizmos.DrawLine(targetPositionAsVec2, ClosestPointToRect(ref _sceneManagerAsset[i].rectangle, targetPositionAsVec2));
+            Gizmos.DrawWireCube(_sceneManagerAsset[i].alteredCenter, new Vector3(_sceneManagerAsset[i].rectangle.width, _sceneManagerAsset[i].rectangle.height));
+        }
     }
 #endif
 }
