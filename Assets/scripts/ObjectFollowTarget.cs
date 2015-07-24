55f17f24d6c996674b87fdef1622f60141c37f02
Ben Griffiths <scare_tfc@hotmail.com>
2015-07-24 21:57:01 +0100

    Updated to Example scenes
    Added ObjectFollowTarget, a very basic script to just move a ball
    towards the player to show that it can traverse across scenes and will
    not disappear when the scene it loaded in with is unloaded.

---
+++ Assets/scripts/ObjectFollowTarget.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectFollowTarget : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _target;
+    private Rigidbody2D _rigidBody;
+
+    private readonly float _stoppingDistance = 1.5f;
+
+    private void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        _target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+	void FixedUpdate ()
+    {
+        var dir = (_target.position - transform.position);
+
+        if (dir.magnitude > _stoppingDistance)
+            _rigidBody.AddForce(dir.normalized * 30 * Time.deltaTime, ForceMode2D.Force);
+	}
+}
