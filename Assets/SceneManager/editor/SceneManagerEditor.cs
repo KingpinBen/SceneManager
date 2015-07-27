b6e5ba517efaa18a735d9289c75edd7e11f38218
Ben Griffiths <scare_tfc@hotmail.com>
2015-07-27 22:25:51 +0100

    Editor to draw load radius in SceneView

---
+++ Assets/SceneManager/editor/SceneManagerEditor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(SceneManager))]
+public class SceneManagerEditor : Editor
+{
+    private SceneManager _target;
+
+    void OnEnable()
+    {
+        _target = target as SceneManager;
+    }
+
+    void OnSceneGUI()
+    {
+        if (!EditorApplication.isPlaying)
+            return;
+
+        Debug.Log("Drawing");
+        Handles.CircleCap(0, _target.targetPositionAsVec2, Quaternion.identity, SceneManager.cBufferArea *.5f);
+    }
+}
